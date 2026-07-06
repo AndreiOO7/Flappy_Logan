f4d1d10c333b3895eb415b4face8c70124665425
sawelyew <savelyevserafim@gmail.com>
2026-07-06 16:02:48 +0300

    Fixed CORS

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -14,7 +14,8 @@ origins = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5500",
-    "https://proactive-reprieve-production-f162.up.railway.app"
+    "https://proactive-reprieve-production-f162.up.railway.app",
+    "http://proactive-reprieve-production-f162.up.railway.app",
 ]
 
 app.add_middleware(
